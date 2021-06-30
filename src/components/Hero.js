cf3a723074c9b77093a973268f239f1e9914e431
Ben Kosa <bkosa2@cs.washington.edu>
2021-06-30 16:53:24 -0700

    Pushing to make sure global version is up to date as of 6/30/2021.

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from "styled-components";
 import {Button} from "./Button";
-import Video from '../assets/videos/pexels-gotta-be-worth-it-1029115.jpg';
+import Video from '../assets/videos/Pexels Videos 2169879.mp4';
 
 // This is not made to be reusable since we only have one hero and the rest of the page is covered with cards.
 // Reuse could be applicable when it comes to having multiple pages???
@@ -9,13 +9,16 @@ const Hero = () => {
     return (
         <HeroContainer>
             <HeroBg>
-                <VideoBg src={ Video } type="video/mp4" />
+                <VideoBg src={ Video } type="video/mp4" autoPlay loop muted playsInLine />
             </HeroBg>
             <HeroContent>
                 <HeroItems>
                     <HeroH1>Unreal Destinations</HeroH1>
                     <HeroP>Out of this world!</HeroP>
-                    <Button>Travel Now</Button>
+                    <Button primary='true' big='true' round='true'
+                            to='/trips'>
+                        Travel Now
+                    </Button>
                 </HeroItems>
             </HeroContent>
         </HeroContainer>
@@ -25,7 +28,7 @@ const Hero = () => {
 export default Hero;
 
 const HeroContainer = styled.div`
-    background: #0c0c0c;
+    background: transparent;
     display: flex;
     justify-content: center;
     align-items: center;
@@ -34,28 +37,73 @@ const HeroContainer = styled.div`
     position: relative;
     margin-top: -80px;
     color: #fff;
+    
+    :before {
+        content: "",
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        z-index: 2;
+        background: linear-gradient(
+            100deg, 
+            rgba(0,0,0,0.2) 0%, 
+            rgba(0,0,0,0,6) 100%,
+        ), linear-gradient(100deg, 
+            rgba(0, 0, 0, 0.2) 0%, 
+            transparent 100%
+        );
 `
 
 const HeroBg = styled.div`
-        
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
 `
 
 const VideoBg = styled.video`
-    
+    width: 100%;
+    height: 100%;
+    -o-object-fit: cover;
+    object-fit: cover;
 `
 
 const HeroContent = styled.div`
-    
+    z-index: 3;
+    height: calc(100vh - 80px);
+    max-height: 100%;
+    padding: 0rem calc((100vw - 1300px) / 2);
 `
 
 const HeroItems = styled.div`
-
+     display: flex;
+     flex-direction: column;
+     justify-content: center;
+     align-items: center;
+     text-align: center;
+     height: 100vh;
+     max-height: 100%;
+     padding: 0;
+     color: #fff;
+     line-height: 1.1;
+     font-weight: bold;
 `
 
 const HeroH1 = styled.h1`
-
+    font-size: clamp(1.5rem, 6vw, 4rem);
+    margin-bottom: 1.5rem;
+    letter-spacing: 3px;
+    padding: 0 1rem;
 `
 
 const HeroP = styled.p`
-    
+    font-size: clamp(1rem, 3vw, 3rem);
+    margin-bottom: 2rem;
+    font-weight: 400;
 `
---
+++ src/components/styles/Testimonials.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import styled from "styled-components";
+import Img from 'gatsby-image';
+import {IoMdCheckmarkCircleOutline} from "react-icons/all";
+
+const Testimonials = () => {
+    return (
+        <TestimonialsContainer>
+            <TopLine>
+                Testimonials
+            </TopLine>
+            <Description>
+                What People are Saying
+            </Description>
+            <ContentWrapper>
+                <ColumnOne>
+                    <Testimonial>
+                        <IoMdCheckmarkCircleOutline />
+                        <h3>Sean Michaels</h3>
+                        <p>"The greatest experience of my life! It was so much fun exploring the mountains and they made it super easy to book my trip and accomodation."</p>
+                    </Testimonial>
+                </ColumnOne>
+            </ContentWrapper>
+        </TestimonialsContainer>
+    );
+};
+
+export default Testimonials;
